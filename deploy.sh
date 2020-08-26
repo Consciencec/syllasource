99d31bc7cb3208c36fbb1457637ac5fc4221e844
Consciencec <firecuser28@gmail.com>
2020-08-26 14:31:35 -0400

    test

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,6 @@ printf "\033[0;32mDeploying updates to Firebase...\033[0m\n"
 firebase deploy -m "firebase deployment #$n"
 printf "\033[0;32mApplying updates to GitLab...\033[0m\n"
 
-# change baseURL to satherws.gitlab.io/satherc_tech, commit then return baseURL to original state
+# GitLab push with incremented commit message
 git add . && git commit -m "firebase deployment #$n"
 git push -u origin master
